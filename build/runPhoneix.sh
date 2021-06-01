d758a91d89c31161925de677feccffd42d394c36
antoniospg <antoniospg100@gmail.com>
2021-06-01 18:58:36 -0300

    relinkando cli args

--- build/runPhoneix.sh
+++ build/runPhoneix.sh
@@ -1,8 +1,8 @@
-local_ip="172.19.59.2"
-vision_port=10020
+local_ip="192.168.0.110"
+vision_port="10020"
 referee_port="10003"
-com_address="172.19.59.2"
-com_port=10301
+com_address="192.168.0.110"
+com_port="10301"
 environment=0 #não usado ainda
 
 ./Phoenix-cli.exe $local_ip $vision_port $referee_port $com_address $com_port
